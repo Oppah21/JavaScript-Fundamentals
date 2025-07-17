153d9adbb8439b6cccf35d643b652e602c75a03b
Oppah21 <matemaoppah@gmail.com>
2025-07-17 11:09:31 +0200

    testig 2

---
+++ javasriptbasics.js
@@ -0,0 +1,44 @@
+let name="oppah"
+console.log(name)
+
+const age=21
+console.log(age)
+
+let isstudent = true
+console.log(isstudent)
+
+let city
+console.log(city)
+const cars = ["Range Rover","BMW","Benz","Lincoln",2,true]
+console.log(cars)
+const person={name:"oppah",
+    surname:"Matema",
+    age:21,
+    location:"Umwinzi",
+    sex:"female",
+    hobbie:["busketball","cooking","hiking"]
+
+}
+console.log(person)
+
+let colors=["red","pink","yellow","green","blue","black"]
+console.log(colors[0])
+
+console.log(colors[2])
+colors.push("violet")
+console.log(colors)
+colors.pop()
+console.log(colors)
+colors.shift()
+console.log(colors)
+colors.unshift("pink")
+console.log(colors)
+colors.length
+console.log(colors.length)
+colors.forEach((b)=>{
+    console.log(b)
+
+})
+colors.map((color )=>{
+    console.log(color)
+})
